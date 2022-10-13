ef72ef8b2cc1276b33fe6a1be059ec8d8803a589
carmelopalumbo <carmelopalumbo@outlook.it>
2022-10-13 14:11:01 +0200

    add-js-structure

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 // js
+
+//const
+const fizzOutput = "Fizz";
+const buzzOutput = "Buzz";
+const fizzbuzzOutput = 'FizzBuzz';
+const limit = 100;
+
+
+// verifica multiplo
+for(let i = 1; i <= limit; i++){
+    if(!(i % 3) && !(i % 5)){
+        console.log(fizzbuzzOutput);
+    }else if(!(i % 5)){
+        console.log(buzzOutput);
+    }else if(!(i % 3)){
+        console.log(fizzOutput);
+    }else{
+        console.log(i);
+    }
+}
