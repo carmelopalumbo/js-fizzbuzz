e18cc9d4d9b71cfc7e1661c57a2792eede507c60
carmelopalumbo <carmelopalumbo@outlook.it>
2022-10-13 14:44:46 +0200

    first-release

--- js/script.js
+++ js/script.js
@@ -3,19 +3,61 @@
 //const
 const fizzOutput = "Fizz";
 const buzzOutput = "Buzz";
-const fizzbuzzOutput = 'FizzBuzz';
+const fizzbuzzOutput = "FizzBuzz";
 const limit = 100;
-
+const container = document.querySelector('.col');
 
 // verifica multiplo
 for(let i = 1; i <= limit; i++){
     if(!(i % 3) && !(i % 5)){
         console.log(fizzbuzzOutput);
+
+        //creo il box
+        const box = document.createElement('div');
+
+        //aggiungo la classe
+        box.className = 'numberbox';
+        box.classList.add('green');
+
+        //output e aggiungo al container
+        box.innerHTML = fizzbuzzOutput;
+        container.append(box);
+
     }else if(!(i % 5)){
         console.log(buzzOutput);
+        const box = document.createElement('div');
+
+        //aggiungo la classe
+        box.className = 'numberbox';
+        box.classList.add('yellow');
+
+        //output e aggiungo al container
+        box.innerHTML = buzzOutput;
+        container.append(box);
+
     }else if(!(i % 3)){
         console.log(fizzOutput);
+         const box = document.createElement('div');
+
+        //aggiungo la classe
+        box.className = 'numberbox';
+        box.classList.add('red');
+
+        //output e aggiungo al container
+        box.innerHTML = fizzOutput;
+        container.append(box);
+
     }else{
         console.log(i);
+
+        const box = document.createElement('div');
+
+        //aggiungo la classe
+        box.className = 'numberbox';
+        box.classList.add('blue');
+
+        //output e aggiungo al container
+        box.innerHTML = i;
+        container.append(box);
     }
 }
